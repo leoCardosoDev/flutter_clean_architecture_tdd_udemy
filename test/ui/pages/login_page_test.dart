33e8a94e0be4e6059cad6730f3f09da0b9e72189
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-23 16:22:12 -0300

    test: ensure LoginPage close all streams with dispose

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -215,4 +215,11 @@ void main() {
 
     expect(find.text('main error'), findsWidgets);
   });
+
+  testWidgets('Should close streams on dispose', (WidgetTester tester) async {
+    await loadPage(tester);
+    addTearDown(() {
+      verify(presenter.dispose()).called(1);
+    });
+  });
 }
