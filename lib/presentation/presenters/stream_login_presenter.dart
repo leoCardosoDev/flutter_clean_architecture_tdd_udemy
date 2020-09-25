f9036a1671beffc2b3fb7003046cbe9905bf2234
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-25 12:25:00 -0300

    test: ensure StreamLoginPresenter calls on password changed

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -20,8 +20,13 @@ class StreamLoginPresenter {
       _controller.stream.map((state) => state.isFormValid).distinct();
 
   StreamLoginPresenter({@required this.validation});
+
   void validateEmail(String email) {
     _state.emailError = validation.validate(field: 'email', value: email);
     _controller.add(_state);
+  }
+
+  void validatePassword(String password) {
+    _state.emailError = validation.validate(field: 'password', value: password);
   }
 }
