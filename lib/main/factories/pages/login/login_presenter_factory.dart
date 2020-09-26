89689dbf173f57fc3b6c16e548b1d2dfc1587a00
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-26 15:28:52 -0300

    refactor: add GetxLoginPresenter

--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -2,8 +2,15 @@ import '../../../../presentation/presenters/presenter.dart';
 import '../../../../ui/pages/pages.dart';
 import '../../factories.dart';
 
-LoginPresenter makeLoginPresenter() {
+LoginPresenter makeStreamLoginPresenter() {
   return StreamLoginPresenter(
+    authentication: makeRemoteAuthentication(),
+    validation: makeLoginValidation(),
+  );
+}
+
+LoginPresenter makeGetxLoginPresenter() {
+  return GetxLoginPresenter(
     authentication: makeRemoteAuthentication(),
     validation: makeLoginValidation(),
   );
