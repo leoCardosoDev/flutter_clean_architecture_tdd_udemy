6024dca0a71206849cdd0807253300a30958f784
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-28 18:19:27 -0300

    refactor: move GetxSplashPresenter to its own file

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,30 +1,10 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_clean_architecture_tdd/domain/entities/account_entity.dart';
-import 'package:meta/meta.dart';
-import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:flutter_clean_architecture_tdd/domain/entities/entities.dart';
 import 'package:flutter_clean_architecture_tdd/domain/usecases/usecases.dart';
-import 'package:flutter_clean_architecture_tdd/ui/pages/pages.dart';
-
-class GetxSplashPresenter implements SplashPresenter {
-  final LoadCurrentAccount loadCurrentAccount;
-
-  var _navigateTo = RxString();
-  Stream<String> get navigateToStream => _navigateTo.stream;
-
-  GetxSplashPresenter({@required this.loadCurrentAccount});
-
-  Future<void> checkAccount() async {
-    try {
-      final account = await loadCurrentAccount.load();
-      _navigateTo.value = account.isNull ? '/login' : '/surveys';
-    } catch (error) {
-      _navigateTo.value = '/login';
-    }
-  }
-}
+import 'package:flutter_clean_architecture_tdd/presentation/presenters/presenter.dart';
 
 class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 
