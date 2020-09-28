ab103bbb68cb09b63919565ad071f513e2c3f992
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-28 19:14:07 -0300

    feat: add delay in GetxSplashPresenter transition

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -30,7 +30,7 @@ void main() {
   });
 
   test('Should call LoadCurrentAccount', () async {
-    await sut.checkAccount();
+    await sut.checkAccount(durationInSeconds: 0);
 
     verify(loadCurrentAccount.load()).called(1);
   });
@@ -38,13 +38,13 @@ void main() {
   test('Should got to surveys page on success', () async {
     sut.navigateToStream
         .listen(expectAsync1((page) => expect(page, '/surveys')));
-    await sut.checkAccount();
+    await sut.checkAccount(durationInSeconds: 0);
   });
 
   test('Should got to login page on null result', () async {
     mockLoadCurrentAccount(account: null);
     sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
-    await sut.checkAccount();
+    await sut.checkAccount(durationInSeconds: 0);
   });
 
   test('Should got to login page on error', () async {
