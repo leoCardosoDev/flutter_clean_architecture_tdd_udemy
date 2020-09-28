ea4fddd109b58b9fea92f503a60aaf66afb07f03
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-28 14:04:28 -0300

    feat: ensure LocalStorageAdapter calls fetchSecure with correct values

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -19,19 +19,28 @@ void main() {
     value = faker.guid.guid();
   });
 
-  void mockSaveSecureError() {
-    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
-        .thenThrow(Exception());
-  }
+  group('saveSecure', () {
+    void mockSaveSecureError() {
+      when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+          .thenThrow(Exception());
+    }
 
-  test('Should call save secure with correct values', () async {
-    await sut.saveSecure(key: key, value: value);
-    verify(secureStorage.write(key: key, value: value));
+    test('Should call save secure with correct values', () async {
+      await sut.saveSecure(key: key, value: value);
+      verify(secureStorage.write(key: key, value: value));
+    });
+
+    test('Should throw if save secure throws', () async {
+      mockSaveSecureError();
+      final future = sut.saveSecure(key: key, value: value);
+      expect(future, throwsA(TypeMatcher<Exception>()));
+    });
   });
 
-  test('Should throw if save secure throws', () async {
-    mockSaveSecureError();
-    final future = sut.saveSecure(key: key, value: value);
-    expect(future, throwsA(TypeMatcher<Exception>()));
+  group('fetchSecure', () {
+    test('Should call fetchSecure with correct values', () async {
+      await sut.fetchSecure(key);
+      verify(secureStorage.read(key: key));
+    });
   });
 }
