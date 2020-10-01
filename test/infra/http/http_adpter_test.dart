a780c06b80912c3ec7d63ecccbc73f325e9855c5
Leonardo Cardoso <leocardosodev@gmail.com>
2020-10-01 17:40:43 -0300

    test: ensure HttpAdapter throws all on 40*

--- test/infra/http/http_adpter_test.dart
+++ test/infra/http/http_adpter_test.dart
@@ -182,6 +182,31 @@ void main() {
       final future = sut.request(url: url, method: 'get');
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    test('Should return UnauthorizedError if get returns 401', () async {
+      mockResponse(401);
+      final future = sut.request(url: url, method: 'get');
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should return ForbiddenError if get returns 403', () async {
+      mockResponse(403);
+      final future = sut.request(url: url, method: 'get');
+      expect(future, throwsA(HttpError.forbidden));
+    });
+
+    test('Should return NotFoundError if get returns 404', () async {
+      mockResponse(404);
+      final future = sut.request(url: url, method: 'get');
+      expect(future, throwsA(HttpError.notFound));
+    });
+
+    test('Should return ServerError if get returns 500', () async {
+      mockResponse(500);
+      final future = sut.request(url: url, method: 'get');
+      expect(future, throwsA(HttpError.serverError));
+    });
+
     //
   });
 }
