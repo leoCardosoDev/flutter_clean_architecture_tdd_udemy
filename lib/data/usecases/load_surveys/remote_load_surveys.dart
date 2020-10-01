0d9f164229b78a464eae6fd11dd4cd40abad0913
Leonardo Cardoso <leocardosodev@gmail.com>
2020-10-01 16:24:59 -0300

    refactor: move RemoteLoadSurveys to production files

---
+++ lib/data/usecases/load_surveys/remote_load_surveys.dart
@@ -0,0 +1,28 @@
+import 'package:meta/meta.dart';
+
+import '../../../domain/entities/entities.dart';
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+
+import '../../../data/models/models.dart';
+import '../../../data/http/http.dart';
+
+class RemoteLoadSurveys implements LoadSurveys {
+  final String url;
+  final HttpClient<List<Map>> httpClient;
+
+  RemoteLoadSurveys({@required this.url, @required this.httpClient});
+
+  Future<List<SurveyEntity>> load() async {
+    try {
+      final httpResponse = await httpClient.request(url: url, method: 'get');
+      return httpResponse
+          .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
+          .toList();
+    } on HttpError catch (error) {
+      throw error == HttpError.forbidden
+          ? DomainError.accessDenied
+          : DomainError.unexpected;
+    }
+  }
+}
