d450b1860a1cb5dc7aa51fcfabffc008c880df64
Leonardo Cardoso <leocardosodev@gmail.com>
2020-10-02 22:46:30 -0300

    test: ensure GetxSurveysPresenter emits correct events on success

--- lib/ui/pages/surveys/survey_view_model.dart
+++ lib/ui/pages/surveys/survey_view_model.dart
@@ -1,10 +1,17 @@
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
-class SurveyViewModel {
+class SurveyViewModel extends Equatable {
   final String id;
   final String question;
   final String date;
   final bool didAnswer;
 
-  SurveyViewModel({@required this.id, @required this.question, @required this.date, @required this.didAnswer});
+  List get props => ['id', 'question', 'date', 'didAnswer'];
+
+  SurveyViewModel(
+      {@required this.id,
+      @required this.question,
+      @required this.date,
+      @required this.didAnswer});
 }
