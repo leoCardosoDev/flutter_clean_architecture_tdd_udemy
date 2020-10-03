b2002c98cc33016955ac2bcea20e446a4fd2667c
Leonardo Cardoso <leocardosodev@gmail.com>
2020-10-03 15:29:19 -0300

    refactor: isolate load test in a group

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -11,108 +11,110 @@ import 'package:flutter_clean_architecture_tdd/data/usecases/usecases.dart';
 class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
 
 void main() {
-  LocalLoadSurveys sut;
-  FetchCacheStorageSpy fetchCacheStorage;
-  List<Map> data;
+  group('LOAD', () {
+    LocalLoadSurveys sut;
+    FetchCacheStorageSpy fetchCacheStorage;
+    List<Map> data;
 
-  PostExpectation mockFetchCall() => when(fetchCacheStorage.fetch(any));
+    PostExpectation mockFetchCall() => when(fetchCacheStorage.fetch(any));
 
-  List<Map> mockValidData() => [
+    List<Map> mockValidData() => [
+          {
+            'id': faker.guid.guid(),
+            'question': faker.randomGenerator.string(10),
+            'date': '2020-07-20T00:00:00Z',
+            'didAnswer': 'false',
+          },
+          {
+            'id': faker.guid.guid(),
+            'question': faker.randomGenerator.string(10),
+            'date': '2019-02-02T00:00:00Z',
+            'didAnswer': 'true',
+          }
+        ];
+    void mockFetch(List<Map> list) {
+      data = list;
+      mockFetchCall().thenAnswer((_) async => data);
+    }
+
+    void mockFetchError() => mockFetchCall().thenThrow(Exception());
+
+    setUp(() {
+      fetchCacheStorage = FetchCacheStorageSpy();
+      sut = LocalLoadSurveys(fetchCacheStorage: fetchCacheStorage);
+      mockFetch(mockValidData());
+    });
+
+    test('Should call FetchCacheStorage with correct key', () async {
+      await sut.load();
+
+      verify(fetchCacheStorage.fetch('surveys')).called(1);
+    });
+
+    test('Should return a list of surveys on success', () async {
+      final surveys = await sut.load();
+
+      expect(surveys, [
+        SurveyEntity(
+            id: data[0]['id'],
+            question: data[0]['question'],
+            dateTime: DateTime.utc(2020, 7, 20),
+            didAnswer: false),
+        SurveyEntity(
+            id: data[1]['id'],
+            question: data[1]['question'],
+            dateTime: DateTime.utc(2019, 2, 2),
+            didAnswer: true),
+      ]);
+    });
+
+    test('Should throw UnexpectedError if cache is empty', () async {
+      mockFetch([]);
+
+      final future = sut.load();
+
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('Should throw UnexpectedError if cache is null', () async {
+      mockFetch(null);
+
+      final future = sut.load();
+
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('Should throw UnexpectedError if cache is invalid', () async {
+      mockFetch([
         {
-          'id': faker.guid.guid(),
-          'question': faker.randomGenerator.string(10),
-          'date': '2020-07-20T00:00:00Z',
-          'didAnswer': 'false',
-        },
-        {
-          'id': faker.guid.guid(),
-          'question': faker.randomGenerator.string(10),
-          'date': '2019-02-02T00:00:00Z',
-          'didAnswer': 'true',
+          'id': 'invalid_id',
+          'question': 'invalid_question',
+          'date': 'invalid_date',
+          'didAnswer': 'invalid_false',
         }
-      ];
-  void mockFetch(List<Map> list) {
-    data = list;
-    mockFetchCall().thenAnswer((_) async => data);
-  }
+      ]);
 
-  void mockFetchError() => mockFetchCall().thenThrow(Exception());
+      final future = sut.load();
 
-  setUp(() {
-    fetchCacheStorage = FetchCacheStorageSpy();
-    sut = LocalLoadSurveys(fetchCacheStorage: fetchCacheStorage);
-    mockFetch(mockValidData());
-  });
+      expect(future, throwsA(DomainError.unexpected));
+    });
 
-  test('Should call FetchCacheStorage with correct key', () async {
-    await sut.load();
+    test('Should throw UnexpectedError if cache is incomplete', () async {
+      mockFetch([
+        {'date': '2020-07-20T00:00:00Z', 'didAnswer': 'false'}
+      ]);
 
-    verify(fetchCacheStorage.fetch('surveys')).called(1);
-  });
+      final future = sut.load();
 
-  test('Should return a list of surveys on success', () async {
-    final surveys = await sut.load();
+      expect(future, throwsA(DomainError.unexpected));
+    });
 
-    expect(surveys, [
-      SurveyEntity(
-          id: data[0]['id'],
-          question: data[0]['question'],
-          dateTime: DateTime.utc(2020, 7, 20),
-          didAnswer: false),
-      SurveyEntity(
-          id: data[1]['id'],
-          question: data[1]['question'],
-          dateTime: DateTime.utc(2019, 2, 2),
-          didAnswer: true),
-    ]);
-  });
+    test('Should throw UnexpectedError if cache throws', () async {
+      mockFetchError();
 
-  test('Should throw UnexpectedError if cache is empty', () async {
-    mockFetch([]);
+      final future = sut.load();
 
-    final future = sut.load();
-
-    expect(future, throwsA(DomainError.unexpected));
-  });
-
-  test('Should throw UnexpectedError if cache is null', () async {
-    mockFetch(null);
-
-    final future = sut.load();
-
-    expect(future, throwsA(DomainError.unexpected));
-  });
-
-  test('Should throw UnexpectedError if cache is invalid', () async {
-    mockFetch([
-      {
-        'id': 'invalid_id',
-        'question': 'invalid_question',
-        'date': 'invalid_date',
-        'didAnswer': 'invalid_false',
-      }
-    ]);
-
-    final future = sut.load();
-
-    expect(future, throwsA(DomainError.unexpected));
-  });
-
-  test('Should throw UnexpectedError if cache is incomplete', () async {
-    mockFetch([
-      {'date': '2020-07-20T00:00:00Z', 'didAnswer': 'false'}
-    ]);
-
-    final future = sut.load();
-
-    expect(future, throwsA(DomainError.unexpected));
-  });
-
-  test('Should throw UnexpectedError if cache throws', () async {
-    mockFetchError();
-
-    final future = sut.load();
-
-    expect(future, throwsA(DomainError.unexpected));
+      expect(future, throwsA(DomainError.unexpected));
+    });
   });
 }
