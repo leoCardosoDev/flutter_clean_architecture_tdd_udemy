16d23ff216495c9834a599f30a10485f588b4a7b
Leonardo Cardoso <leocardosodev@gmail.com>
2020-10-03 14:28:46 -0300

    test: ensure LocalLoadSurveys throws UnexpectedError if cache is incomplet

--- lib/data/models/local_survey_model.dart
+++ lib/data/models/local_survey_model.dart
@@ -1,4 +1,5 @@
 import 'package:meta/meta.dart';
+
 import '../../domain/entities/entities.dart';
 
 class LocalSurveyModel {
@@ -15,6 +16,11 @@ class LocalSurveyModel {
   });
 
   factory LocalSurveyModel.fromJson(Map json) {
+    if (!json.keys
+        .toSet()
+        .containsAll(['id', 'question', 'date', 'didAnswer'])) {
+      throw Exception();
+    }
     return LocalSurveyModel(
       id: json['id'],
       question: json['question'],
