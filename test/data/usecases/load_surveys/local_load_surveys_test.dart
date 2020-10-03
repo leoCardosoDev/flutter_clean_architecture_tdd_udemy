0b5c13e67f352654d92355f381eaad01cbd609b8
Leonardo Cardoso <leocardosodev@gmail.com>
2020-10-03 14:46:48 -0300

    refactor: move LocalLoadSurveys and FetchCacheStorage to own files

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -1,38 +1,14 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_clean_architecture_tdd/data/models/models.dart';
-import 'package:flutter_clean_architecture_tdd/domain/entities/survey_entity.dart';
-import 'package:flutter_clean_architecture_tdd/domain/helpers/helpers.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-import 'package:meta/meta.dart';
+import 'package:flutter_clean_architecture_tdd/domain/entities/survey_entity.dart';
+import 'package:flutter_clean_architecture_tdd/domain/helpers/helpers.dart';
 
-class LocalLoadSurveys {
-  final FetchCacheStorage fetchCacheStorage;
-
-  LocalLoadSurveys({@required this.fetchCacheStorage});
-
-  Future<List<SurveyEntity>> load() async {
-    try {
-      final data = await fetchCacheStorage.fetch('surveys');
-      if (data?.isEmpty != false) {
-        throw Exception();
-      }
-      return data
-          .map<SurveyEntity>(
-              (json) => LocalSurveyModel.fromJson(json).toEntity())
-          .toList();
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
+import 'package:flutter_clean_architecture_tdd/data/cache/cache.dart';
+import 'package:flutter_clean_architecture_tdd/data/usecases/usecases.dart';
 
 class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
-
-abstract class FetchCacheStorage {
-  Future<dynamic> fetch(String key);
-}
 
 void main() {
   LocalLoadSurveys sut;
