8529e1f3ddf42171adbfb4a288210793258958bf
Leonardo Silva <leonardo.silva@simpaul.com.br>
2021-03-22 18:54:27 -0300

    feat: add result Apge

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -18,7 +18,7 @@ class App extends StatelessWidget {
       title: '4Dev',
       debugShowCheckedModeBanner: false,
       theme: makeAppTheme(),
-      initialRoute: '/',
+      initialRoute: '/survey_result/3',
       getPages: [
         GetPage(name: '/', page: makeSplashPage, transition: Transition.fade),
         GetPage(
@@ -28,6 +28,7 @@ class App extends StatelessWidget {
             name: '/surveys',
             page: makeSurveysPage,
             transition: Transition.fadeIn),
+        GetPage(name: '/survey_result/:survey_id', page: makeSurveyResultPage),
       ],
     );
   }
