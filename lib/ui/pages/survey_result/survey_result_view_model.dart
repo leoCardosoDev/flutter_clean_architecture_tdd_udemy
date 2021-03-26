d927944e75ddff8134b4c7803679c4be8f0a079d
Leonardo Silva <leonardo.silva@simpaul.com.br>
2021-03-26 12:30:56 -0300

    feat: add GetxSurveyResultPresenter with test

--- lib/ui/pages/survey_result/survey_result_view_model.dart
+++ lib/ui/pages/survey_result/survey_result_view_model.dart
@@ -1,8 +1,9 @@
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 import './survey_answer_view_model.dart';
 
-class SurveyResultViewModel {
+class SurveyResultViewModel extends Equatable {
   final String surveyId;
   final String question;
   final List<SurveyAnswerViewModel> answers;
---
+++ test/presentation/presenters/getx_survey_result_presenter_test.dart
@@ -0,0 +1,99 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:flutter_clean_architecture_tdd/ui/helpers/helpers.dart';
+import 'package:flutter_clean_architecture_tdd/ui/pages/pages.dart';
+
+import 'package:flutter_clean_architecture_tdd/domain/entities/entities.dart';
+import 'package:flutter_clean_architecture_tdd/domain/helpers/helpers.dart';
+import 'package:flutter_clean_architecture_tdd/domain/usecases/usecases.dart';
+
+import 'package:flutter_clean_architecture_tdd/presentation/presenters/presenter.dart';
+
+class LoadSurveyResultSpy extends Mock implements LoadSurveyResult {}
+
+void main() {
+  GetxSurveyResultPresenter sut;
+  LoadSurveyResultSpy loadSurveyResult;
+  SurveyResultEntity loadResult;
+  String surveyId;
+
+  SurveyResultEntity mockValidData() => SurveyResultEntity(
+          surveyId: faker.guid.guid(),
+          question: faker.lorem.sentence(),
+          answers: [
+            SurveyAnswerEntity(
+              image: faker.internet.httpUrl(),
+              answer: faker.lorem.sentence(),
+              percent: faker.randomGenerator.integer(100),
+              isCurrentAnswer: faker.randomGenerator.boolean(),
+            ),
+            SurveyAnswerEntity(
+              answer: faker.lorem.sentence(),
+              percent: faker.randomGenerator.integer(100),
+              isCurrentAnswer: faker.randomGenerator.boolean(),
+            ),
+          ]);
+
+  PostExpectation mockLoadSurveyResultCall() =>
+      when(loadSurveyResult.loadBySurvey(surveyId: anyNamed('surveyId')));
+
+  void mockLoadSurveyResult(SurveyResultEntity data) {
+    loadResult = data;
+    mockLoadSurveyResultCall().thenAnswer((_) async => loadResult);
+  }
+
+  void mockLoadSurveyResultError() =>
+      mockLoadSurveyResultCall().thenThrow(DomainError.unexpected);
+
+  setUp(() {
+    surveyId = faker.guid.guid();
+    loadSurveyResult = LoadSurveyResultSpy();
+    sut = GetxSurveyResultPresenter(
+      loadSurveyResult: loadSurveyResult,
+      surveyId: surveyId,
+    );
+    mockLoadSurveyResult(mockValidData());
+  });
+
+  test('Should call LoadSurveyResult on loadData', () async {
+    await sut.loadData();
+    verify(loadSurveyResult.loadBySurvey(surveyId: surveyId)).called(1);
+  });
+
+  test('Should emit correct events on success', () async {
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.surveyResultStream.listen(expectAsync1((result) => expect(
+        result,
+        SurveyResultViewModel(
+          surveyId: loadResult.surveyId,
+          question: loadResult.question,
+          answers: [
+            SurveyAnswerViewModel(
+              image: loadResult.answers[0].image,
+              answer: loadResult.answers[0].answer,
+              isCurrentAnswer: loadResult.answers[0].isCurrentAnswer,
+              percent: '${loadResult.answers[0].percent}%',
+            ),
+            SurveyAnswerViewModel(
+              answer: loadResult.answers[1].answer,
+              isCurrentAnswer: loadResult.answers[1].isCurrentAnswer,
+              percent: '${loadResult.answers[1].percent}%',
+            ),
+          ],
+        ))));
+    await sut.loadData();
+  });
+
+  test('Should emit correct events on failure', () async {
+    mockLoadSurveyResultError();
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.surveyResultStream.listen(null,
+        onError: (expectAsync1(
+            (error) => expect(error, UiError.unexpected.description))));
+
+    await sut.loadData();
+  });
+}
